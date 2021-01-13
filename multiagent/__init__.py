8d363921a1ec5fecc070dcf886a2bb4918df9d3a
Neale <nealevanstrn@gmail.com>
2021-01-13 15:46:02 -0600

    Modified the registration process for the environments. Included Pred-Prey environment.

--- multiagent/__init__.py
+++ multiagent/__init__.py
@@ -1,18 +1,39 @@
 from gym.envs.registration import register
 
+import multiagent.scenarios as scenarios
+
 # Multiagent envs
 # ----------------------------------------
 
+scenario = scenarios.load("simple.py").Scenario()
 register(
     id='MultiagentSimple-v0',
-    entry_point='multiagent.envs:SimpleEnv',
-    # FIXME(cathywu) currently has to be exactly max_path_length parameters in
-    # rllab run script
+    entry_point='multiagent.environment:MultiAgentEnv',
     max_episode_steps=100,
+    kwargs={"world":scenario.make_world(),
+        "reset_callback":scenario.reset_world,
+        "reward_callback":scenario.reward,
+        "observation_callback":scenario.observation}
 )
 
+scenario = scenarios.load("simple_speaker_listener.py").Scenario()
 register(
     id='MultiagentSimpleSpeakerListener-v0',
-    entry_point='multiagent.envs:SimpleSpeakerListenerEnv',
+    entry_point='multiagent.environment:MultiAgentEnv',
     max_episode_steps=100,
+    kwargs={"world":scenario.make_world(),
+        "reset_callback":scenario.reset_world,
+        "reward_callback":scenario.reward,
+        "observation_callback":scenario.observation}
+)
+
+scenario = scenarios.load("simple_tag.py").Scenario()
+register(
+    id='PredPrey-v0',
+    entry_point='multiagent.environment:MultiAgentEnv',
+    max_episode_steps=100,
+    kwargs={"world":scenario.make_world(),
+        "reset_callback":scenario.reset_world,
+        "reward_callback":scenario.reward,
+        "observation_callback":scenario.observation}
 )
